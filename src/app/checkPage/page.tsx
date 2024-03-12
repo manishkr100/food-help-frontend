98a3a3c401ed94e4c42681ed15d4041182aee404
Manish Kumar <manish.kumar@MB-M-MANISHK.local>
2024-03-13 01:37:43 +0530

    some changes in CheckPage

--- src/app/checkPage/page.tsx
+++ src/app/checkPage/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const CheckPage = () => {
   return (
-    <div className='bg-orange-500'>CheckPage</div>
+    <div className='bg-orange-500'>
+      some changes in Check Page
+    </div>
   )
 }
 
